fa76f78c4bd3035ce1fb0ec5bb10a0f37ccb805a
Jesus3012 <109054986+Jesus3012@users.noreply.github.com>
2026-06-10 19:30:20 -0600

    Corrección

--- includes/mercadopago_config.php
+++ includes/mercadopago_config.php
@@ -1,6 +1,6 @@
 <?php
 // includes/mercadopago_config.php
 // IMPORTANTE: no subas este archivo con el Access Token real a GitHub.
-define('MP_ACCESS_TOKEN', 'APP_USR-5864631040986073-031901-7746139b15df1b4cee5583d01e27313b-669932070');
-define('MP_TERMINAL_ID', 'NEWLAND_N950__N950NCC804255066'); // Ej: NEWLAND_N950__N950XXXXXXXX
+define('MP_ACCESS_TOKEN', 'APP_USR-');
+define('MP_TERMINAL_ID', 'NEWLAND_N950__'); // Ej: NEWLAND_N950__N950XXXXXXXX
 define('MP_PRINT_ON_TERMINAL', 'no_ticket');
